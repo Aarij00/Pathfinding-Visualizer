1851a23cb8d752f9424369417fd287c438a8ebce
Aarij Ashar <90587576+Aarij00@users.noreply.github.com>
2022-12-23 22:10:48 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -20,6 +20,10 @@ start_button_rect = pygame.rect.Rect(210, 75, 100, 30)
 clear_button_text = constants.SMALL_FONT.render('CLEAR', False, constants.CYAN)
 clear_button_rect = pygame.rect.Rect(890, 75, 100, 30)
 
+no_sol_text = constants.LARGE_FONT.render('No Solution!', False, constants.CYAN)
+no_sol_rect = pygame.rect.Rect(550, 25, 100, 30)
+
+
 
 def draw_toolbar(win: pygame.surface.Surface, menu) -> None:
     pygame.draw.line(win, constants.CYAN, (0, 0), (constants.WIDTH, 0), 3)                                              # TOP
@@ -34,10 +38,17 @@ def draw_toolbar(win: pygame.surface.Surface, menu) -> None:
     pygame.draw.rect(win, constants.CYAN, clear_button_rect, 2)
     win.blit(clear_button_text, (((clear_button_rect.x + (clear_button_rect.w / 2)) - (clear_button_text.get_width() / 2)), 
     ((clear_button_rect.y + (clear_button_rect.h / 2)) - (clear_button_text.get_height() / 2))))
+    
+    if no_sol:
+        pygame.draw.rect(screen, constants.NAVY_BLUE, no_sol_rect, 0)
+        screen.blit(no_sol_text, (((no_sol_rect.x + (no_sol_rect.w / 2)) - (no_sol_text.get_width() / 2)), 
+        ((no_sol_rect.y + (no_sol_rect.h / 2)) - (no_sol_text.get_height() / 2))))
 
     menu.draw(win)
 
 def draw_grid(win: pygame.surface.Surface, maze: Maze) -> None:
+    pygame.draw.rect(win, constants.GREEN, start_node_rect)
+    pygame.draw.rect(win, constants.RED, goal_node_rect)
     for row in range(maze.height):
         for col in range(maze.width):
             maze.grid[row][col].draw(win)
@@ -58,7 +69,7 @@ def runAlgo(win: pygame.surface.Surface, maze: Maze, menu) -> None:
     for row in range(maze.height):
         for col in range(maze.width):
             cell = maze.grid[row][col]
-            if cell.getExplored():
+            if cell.getExplored() and not cell.getStart() and not cell.getGoal():
                 cell.setEmpty()
     maze.solve(lambda: draw_window(win, maze, menu), clock, menu)
 
@@ -70,6 +81,7 @@ def draw_window(win: pygame.surface.Surface, m: Maze, menu: Menu) -> None:
 
 
 def clearGrid(maze: Maze) -> None:
+    no_sol = False
     for row in range(maze.height):
         for col in range(maze.width):
             cell = maze.grid[row][col]
@@ -96,8 +108,10 @@ def makeWallClick(maze):
             cell.setEmpty()
     
 wallDrag = False
+startNodeDrag = False
+goalNodeDrag = False
 def handle_event(win: pygame.surface.Surface, e: pygame.event.Event, maze: Maze, menu: Menu) -> None:
-    global wallDrag
+    global wallDrag, startNodeDrag, goalNodeDrag, offset_x, offset_y
     if e.type == pygame.MOUSEBUTTONDOWN:
         if e.button == 1:
             menu.checkCollision(e.pos)
@@ -107,16 +121,48 @@ def handle_event(win: pygame.surface.Surface, e: pygame.event.Event, maze: Maze,
                 clearGrid(maze)
             elif menu.arrow_rect.collidepoint(e.pos):
                 menu.setExpand(not menu.getExpand())
-                # maze.algorithm = menu.checkCollision(e.pos)
-                # print(maze.algorithm)
+            elif start_node_rect.collidepoint(e.pos):
+                startNodeDrag = True
+                mouse_x, mouse_y = event.pos
+                offset_x = start_node_rect.x - mouse_x
+                offset_y = start_node_rect.y - mouse_y
+            elif goal_node_rect.collidepoint(e.pos):
+                goalNodeDrag = True
+                mouse_x, mouse_y = event.pos
+                offset_x = goal_node_rect.x - mouse_x
+                offset_y = goal_node_rect.y - mouse_y
             else:
                 wallDrag = True
                 makeWallClick(maze)
     elif e.type == pygame.MOUSEBUTTONUP:
+        if startNodeDrag:
+            startNodeDrag = False
+            start_node_rect.x, start_node_rect.y = get_closest_cell((start_node_rect.x, start_node_rect.y))
+            maze.setStartState(get_clicked_cell((start_node_rect.x, start_node_rect.y)))
+        if goalNodeDrag:
+            goalNodeDrag = False
+            goal_node_rect.x, goal_node_rect.y = get_closest_cell((goal_node_rect.x, goal_node_rect.y))
+            maze.setGoalState(get_clicked_cell((goal_node_rect.x, goal_node_rect.y)))
         wallDrag = False
     elif e.type == pygame.MOUSEMOTION:
+        if startNodeDrag:
+            mouse_x, mouse_y = event.pos
+            start_node_rect.x = mouse_x + offset_x
+            start_node_rect.y = mouse_y + offset_y 
+        if goalNodeDrag:
+            mouse_x, mouse_y = event.pos
+            goal_node_rect.x = mouse_x + offset_x
+            goal_node_rect.y = mouse_y + offset_y 
         makeWallDrag(wallDrag, maze)
     
+def get_closest_cell(pos):
+    cx, cy = pos
+    new_x = round(cx / 20, None) * 20
+    y = round(((cy - 200) / 20), None)
+    new_y = 200 + (y * 20)
+    if new_y < 200:
+        new_y = 200
+    return (new_x, new_y)
 
 def get_clicked_cell(pos: tuple[int, int]) -> tuple[int, int]:
     x, y = pos
@@ -126,13 +172,16 @@ def get_clicked_cell(pos: tuple[int, int]) -> tuple[int, int]:
 
 
 myMaze = Maze()
+start_node_rect = pygame.rect.Rect(myMaze.start[1]*constants.SIZE, 200+(myMaze.start[0]*constants.SIZE), 20, 20)
+goal_node_rect = pygame.rect.Rect(myMaze.goal[1]*constants.SIZE, 200+(myMaze.goal[0]*constants.SIZE), 20, 20)
+no_sol = False
 menu = Menu()
 running = True
 clock = pygame.time.Clock()
 while running:
     if myMaze.running == False and myMaze.solution is None:
-        print("*** NO SOLUTION ***")
-        running = False
+        print('no sol')
+        no_sol = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
